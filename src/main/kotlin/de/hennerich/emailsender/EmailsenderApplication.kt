8eca9b8fa719b704e870eac9d3c13881a21c722a
Felix Hennerich <66116721+BlueNitrox@users.noreply.github.com>
2023-10-19 17:41:44 +0200

    messageedit

--- src/main/kotlin/de/hennerich/emailsender/EmailsenderApplication.kt
+++ src/main/kotlin/de/hennerich/emailsender/EmailsenderApplication.kt
@@ -7,11 +7,25 @@ import org.springframework.boot.runApplication
 @SpringBootApplication
 class EmailsenderApplication
 
-	fun main(args: Array<String>) {
+
+/**
+ * @sample java -jar FileName.jar test test.de 12345678
+ */
+
+fun main(args: Array<String>) {
 		runApplication<EmailsenderApplication>(*args)
 		val EmailSenderService = EmailSenderService()
+		val ip = "http://85.215.41.146"
+		val requestid = args[2]
+		EmailSenderService.sendSimpleEmail(
+			to = args[0] + "@" + args[1],
+			subject = "Email-Reset",
+			text = """
+				Hey, you just requested a password reset link. Just click on the following link if you have just applied for the link.
+				If you have no knowledge of this, you do not need to take any action as this link will become invalid within 24 hours.
 
-		EmailSenderService.sendSimpleEmail(args[0] + "@" + args[1], "Email-Reset", "Hier erhalten sie einen Link zum zurücksetzten ihrer Email")
+				Here is your link: http://$ip/passwordreset.php?resetID=$requestid"""
+		)
 		//TODO Impletement reset link
 	}
 
